45a7ee3043051dc883569bea490606b6f0ccaf3e
Younggi Kim <younggi.kim@cre.ma>
2019-03-30 21:59:29 +0900

    [RR] Introducing React Router

--- Modern_React_and_Redux/Section16.md
+++ Modern_React_and_Redux/Section16.md
@@ -12,3 +12,14 @@
 * Need to handle forms in Redux
 * Need to master CRUD operations in React/Redux
 * Errors will likely occur! Need good error handling
+
+## Introducing React Router
+* react-router family
+  - `react-router`: Core navigation lib
+  - `react-router-dom`: Navigation for dom-based-apps
+  - `react-router-native`: Navigation for react-native apps
+  - `react-router-redux`: Bindings between Redux and React Router
+* Add `react-router-dom`
+  ```
+  npm install --save react-router-dom
+  ```
--- Modern_React_and_Redux/streams/client/src/components/App.js
+++ Modern_React_and_Redux/streams/client/src/components/App.js
@@ -1,7 +1,33 @@
 import React from 'react';
+// import react-router
+// BrowserRouter: react component
+import { BrowserRouter, Route } from 'react-router-dom';
+
+// dummy components
+const PageOne = () => {
+  return <div>PageOne</div>;
+};
+
+const PageTwo = () => {
+  return (
+    <div>
+      PageTwo
+      <button>Click Me!</button>
+    </div>
+  );
+};
 
 const App = () => {
-  return <div>App</div>
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/pagetwo" component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
