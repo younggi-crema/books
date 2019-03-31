be960ee397bd0c0337a907cc0d36a60c9a64bdd2
Younggi Kim <younggi.kim@cre.ma>
2019-03-31 19:31:51 +0900

    [RR] Memory Router

--- Modern_React_and_Redux/streams/client/src/components/App.js
+++ Modern_React_and_Redux/streams/client/src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import react-router
 // BrowserRouter: react component
-import { HashRouter, Route, Link } from 'react-router-dom';
+import { MemoryRouter, Route, Link } from 'react-router-dom';
 
 // dummy components
 const PageOne = () => {
@@ -26,12 +26,12 @@ const PageTwo = () => {
 const App = () => {
   return (
     <div>
-      <HashRouter>
+      <MemoryRouter>
         <div>
           <Route path="/" exact component={PageOne} />
           <Route path="/pagetwo" component={PageTwo} />
         </div>
-      </HashRouter>
+      </MemoryRouter>
     </div>
   );
 };
